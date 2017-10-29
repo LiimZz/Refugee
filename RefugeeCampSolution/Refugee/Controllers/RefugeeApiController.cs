2d7060f9a2068f2db40da7909b51ae8d6804cd1b
Slim Khemiri <slim.khmiri@esprit.tn>
2017-10-30 00:46:58 +0100

    slim api

---
+++ RefugeeCampSolution/Refugee/Controllers/RefugeeApiController.cs
@@ -0,0 +1,45 @@
+﻿using Refugee.Data;
+using Refugee.Domain.Entities;
+using Refugee.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Refugee.Controllers
+{
+    public class RefugeeApiController : ApiController
+    {
+        IRefugService rs = null;
+        RefugeeDbContext rd;
+        // GET: api/RefugeeApi
+        public IEnumerable<Refug> Get()
+        {
+            rs = new RefugService();
+            return rs.GetAllRefug();
+        }
+
+        // GET: api/RefugeeApi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/RefugeeApi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/RefugeeApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/RefugeeApi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
